c12efad5fd621899fc469e24c3e38dcdfc39714d
Wffv9FNa <63041557+Wffv9FNa@users.noreply.github.com>
2023-04-29 14:19:08 +0100

    Update webp_to_gif.py
    Script now contains output

--- webp_to_gif.py
+++ webp_to_gif.py
@@ -1,5 +1,9 @@
 import os
 from PIL import Image
+from colorama import init, Fore, Style
+
+# Initialize colorama
+init()
 
 # Get a list of all WebP files in the current directory
 webp_files = [f for f in os.listdir('.') if f.endswith('.webp')]
@@ -18,3 +22,6 @@ for webp_file in webp_files:
 
     # Delete the original WebP file
     os.remove(webp_file)
+
+    # Print output message with original file name in yellow and new file name in cyan
+    print(Fore.YELLOW + f'{webp_file}' + Style.RESET_ALL + ' converted to ' + Fore.CYAN + f'{gif_file}' + Style.RESET_ALL)
