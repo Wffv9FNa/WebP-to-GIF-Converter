42845adc09f8ea814c24b739dea1fde005d0b72f
unknown <github_general_719@mcilvaney.co.uk>
2024-01-15 13:31:47 +0000

    style: fix formatting and quotes

--- webp_to_gif.py
+++ webp_to_gif.py
@@ -6,7 +6,7 @@ from colorama import init, Fore, Style
 init()
 
 # Get a list of all WebP files in the current directory
-webp_files = [f for f in os.listdir('.') if f.endswith('.webp')]
+webp_files = [f for f in os.listdir(".") if f.endswith(".webp")]
 
 # Loop through each WebP file and convert it to GIF
 for webp_file in webp_files:
@@ -14,14 +14,22 @@ for webp_file in webp_files:
     img = Image.open(webp_file)
 
     # Remove the background info if present
-    img.info.pop('background', None)
+    img.info.pop("background", None)
 
     # Save the file as GIF with disposal mode 2
-    gif_file = os.path.splitext(webp_file)[0] + '.gif'
+    gif_file = os.path.splitext(webp_file)[0] + ".gif"
     img.save(gif_file, save_all=True, disposal=2)
 
     # Delete the original WebP file
     os.remove(webp_file)
 
     # Print output message with original file name in yellow and new file name in cyan
-    print(Fore.YELLOW + f'{webp_file}' + Style.RESET_ALL + ' converted to ' + Fore.CYAN + f'{gif_file}' + Style.RESET_ALL)
+    print(
+        Fore.YELLOW
+        + f"{webp_file}"
+        + Style.RESET_ALL
+        + " converted to "
+        + Fore.CYAN
+        + f"{gif_file}"
+        + Style.RESET_ALL
+    )
